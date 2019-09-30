029ae6330119a7f1469b7c672f74a0a46cbd6a9e
giahantran <43287445+giahantran@users.noreply.github.com>
2019-09-30 21:38:00 +0700

    Update 30/9

---
+++ UPDATE/TestCode/Gamecaro.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace GameCaro
+{
+	public partial class Form1 : Form
+	{
+		XuLyBanCo BanCo;
+
+        Random Random = new Random();
+
+        private int sound = 0;
+
+        public Form1()
+		{
+			InitializeComponent();
+
+			BanCo = new XuLyBanCo(pnlChessBoard, txbPlayerName, pctbMark);
+
+			BanCo.VeBanCo();
+			NhacNen.Play();
+		}
+
+		private void tmnote_Tick(object sender, EventArgs e)
+		{
+			NOTE.ForeColor = Color.FromArgb(Random.Next(0, 225), Random.Next(0, 225), Random.Next(0, 225));
+		}
+
+        private string nhac = "Music ON";
+		private SoundPlayer NhacNen = new SoundPlayer(@"NhacNen.wav");
+
+		private void btnNhacNen_Click(object sender, EventArgs e)
+		{
+			if (sound == 1)
+			{
+				NhacNen.Play();
+				sound = 0;
+				btnNhacNen.Text = nhac;
+			}
+			else
+			{
+				NhacNen.Stop();
+				sound = 1;
+				btnNhacNen.Text = "Music OFF";
+			}
+		}
+
+		private void btnthoat_Click(object sender, EventArgs e)
+		{
+           FormThoat f = new FormThoat();
+            f.Show();
+		}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.ActiveControl = btnnewgame;
+            XuLyBanCo.Chan = 0;
+            XuLyBanCo.time = 30;
+        }
+
+        private void btnhuongdan_Click(object sender, EventArgs e)
+        {
+            FormHuongDan f = new FormHuongDan();
+            f.Show();
+        }
+
+        private void btnChanhaidau_Click(object sender, EventArgs e)
+        {
+            if (XuLyBanCo.Chan == 0)
+            {
+                XuLyBanCo.Chan = 1;
+                MessageBox.Show("Đã áp dụng luật chặn hai đầu!");
+                btnChanhaidau.Text = "Hủy luật chặn!";
+            }
+            else
+            {
+                XuLyBanCo.Chan = 0;
+                MessageBox.Show("Đã Hủy luật chặn hai đầu!");
+                btnChanhaidau.Text = "Chặn hai đầu!";
+            }
+        }
+    }
+}
+;
